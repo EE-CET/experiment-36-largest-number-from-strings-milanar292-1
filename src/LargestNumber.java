536a5067faee643fea48cb4a28cb2dd6f166266e
milanar292 <milanar292@gmail.com>
2026-04-07 22:12:22 +0530

    Update LargestNumber.java

--- src/LargestNumber.java
+++ src/LargestNumber.java
@@ -3,12 +3,38 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class LargestNumber {
-   
-        // TODO: Read N
-        // TODO: Read N strings into an array
-        // TODO: Sort the strings using a custom comparator
-        // Comparator logic: (str1, str2) -> (str2 + str1).compareTo(str1 + str2)
-        // TODO: Handle edge case where all numbers are "0" (e.g. input 0 0 -> output "0")
-        // TODO: Concatenate sorted strings and print
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // Read N
+        int n = sc.nextInt();
+        
+        // Read N strings
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.next();
+        }
+        
+        // Custom sort
+        Arrays.sort(arr, new Comparator<String>() {
+            public int compare(String a, String b) {
+                return (b + a).compareTo(a + b);
+            }
+        });
+        
+        // Edge case: all zeros
+        if (arr[0].equals("0")) {
+            System.out.println("0");
+            return;
+        }
+        
+        // Concatenate result
+        StringBuilder result = new StringBuilder();
+        for (String s : arr) {
+            result.append(s);
+        }
+        
+        // Print result
+        System.out.println(result.toString());
+    }
 }
